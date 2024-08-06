9a76ae57704cd17b1539fe6d0e505d1e905ce3ac
Shreyasi Dutta <shredtta@gmail.com>
2024-08-06 19:40:23 +0530

    added list

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./home.css";
+import List from "../components/list/List.jsx";
+
 
 const Home = () => {
   return (
     <div className="home">
-      
+      <List/>
+      <List/>
+      <List/>
+      <List/>
     </div>
   )
 }
