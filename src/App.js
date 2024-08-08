530f84e673430a02e81469460d8d0963825bb602
Shreyasi Dutta <shredtta@gmail.com>
2024-08-08 19:47:55 +0530

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Register from "./pages/register/Register";
+//import Register from "./pages/register/Register";
+import Home from './home/Home.jsx';
 
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <Home/>
     </div>
   );
 }
